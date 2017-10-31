ac822448e9460b386480e85ed348548fc05bd1ab
YovCuapioBc <yovcuapio@gmail.com>
2017-10-31 10:52:28 -0600

    actualizando la estrustura app_1

--- ejercicio 2_validar_tarjeta/js/app.js
+++ ejercicio 2_validar_tarjeta/js/app.js
@@ -1,10 +1,12 @@
+//Solicitar al usuario un número de tarjeta bancaria
 var card = prompt('Ingresa un número de tarjeta');
-
+//Declarando la funcion que contendra el bloque de codigo
 var isValidCard = function(array){
   var digits = [];
   digits.reverse();
-  
+//se genera un for para contar los indices de la tarjeta 
     for(var i=0; i >= card.length; i++){
+//Aqui utilizamos la algoritmo de Lunh    
       if(i % 2){
         var couple = [digits] * 2;
         
